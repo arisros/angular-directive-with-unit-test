f3c3cff8915da53a1cf4a36f891271298bf2323d
arisros <arisjiratkurniawan@gmail.com>
2016-08-24 10:57:05 +0700

    Old Check

--- js/box-directive.js
+++ js/box-directive.js
@@ -3,21 +3,46 @@
 // box-counter resource="api/update-stage-today.json" title="Update Stage Today" foot-text="not should this" class="orange"> 
 // box-counter resource="api/tender-you-bid.json" title="Tender You Bid" footText="Active Tender" class="orange"> 
 
-angular.module('app').directive('boxCounter', function($http) {
+angular.module('app').directive('boxCounter', boxCounter);
+
+boxCounter.$inject = ['$http', 'TenderService'];
+
+function boxCounter($http, TenderService) {
 	return {
 		restrict: 'AE',
 		templateUrl: 'templates/box.html',
 		scope: {
-			title: '=',
+			title: '@',
 			footText: '='
 		},
-		link: function(scope, element, attributes) {
-			scope.icon = attributes.icon;
-			if(scope.icon == 'graph') { scope.graph=true; }
-      $http.get(attributes.resource).success(function(response) {
-        scope.rows = response.data;
-        console.log(scope.rows);
-      });
-    }
+		bindToController: true,
+		link: linkFn
 	};
-});
+
+	function linkFn(scope, element, attributes) {
+		scope.icon = attributes.icon;
+		if (scope.icon === 'graph') {
+			scope.graph = true;
+		}
+		TenderService.getNewTenderCount();
+		/**
+		 * Get http resources of attributes "resource"
+		 * @param  {[type]} scope.rows
+		 * @return {[type]} 
+		 */
+    $http
+    	.get(attributes.resource)
+    	.success(successFn);
+
+
+
+
+
+
+
+
+    function successFn(response) {
+      scope.rows = response.data;
+    }
+  }
+}
