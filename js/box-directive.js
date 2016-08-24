f96e1d1c8920a741b8e6ea11feccb2fb9b3a4b20
arisros <arisjiratkurniawan@gmail.com>
2016-08-24 16:27:16 +0700

    i dontknow

--- js/box-directive.js
+++ js/box-directive.js
@@ -3,21 +3,38 @@
 // box-counter resource="api/update-stage-today.json" title="Update Stage Today" foot-text="not should this" class="orange"> 
 // box-counter resource="api/tender-you-bid.json" title="Tender You Bid" footText="Active Tender" class="orange"> 
 
-angular.module('app').directive('boxCounter', function($http) {
-	return {
-		restrict: 'AE',
-		templateUrl: 'templates/box.html',
-		scope: {
-			title: '=',
-			footText: '='
-		},
-		link: function(scope, element, attributes) {
-			scope.icon = attributes.icon;
-			if(scope.icon == 'graph') { scope.graph=true; }
-      $http.get(attributes.resource).success(function(response) {
-        scope.rows = response.data;
-        console.log(scope.rows);
-      });
-    }
-	};
-});
+angular
+	.module('app')
+	.directive('boxCounter', boxCounter);
+
+	// injector
+	boxCounter.$inject = ['$http'];
+
+	// fn
+	function boxCounter () {
+	
+		var fn = {
+			restrict: 'AE',
+			templateUrl: 'templates/box.html',
+			scope: {
+				title: '=',
+				footText: '='
+			},
+			link: function(scope, element, attributes) {
+				scope.icon = attributes.icon;
+				if(scope.icon == 'graph') { scope.graph=true; }
+	      $http.get(attributes.resource).success(function(response) {
+	        scope.rows = response.data;
+	        console.log(scope.rows);
+	      });
+	    }
+  	};
+  return fn;
+}
+
+function () {
+var a = a;
+
+
+}
+
